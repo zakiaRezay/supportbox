69b580c348219494b43b292d3ac07c3aac8ead26
ZAKIAHUJJAT\zakia <zakiarezaye009@gmail.com>
2025-03-14 12:45:49 +0100

    fresh start

---
+++ src/main/java/support_circle/supportbox/SupportboxApplication.java
@@ -0,0 +1,13 @@
+package support_circle.supportbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/support_circle/supportbox/SupportboxApplicationTests.java
@@ -0,0 +1,13 @@
+package support_circle.supportbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
